e1fdbd9c9e66fc80638dea7f6ee5fce6881d31af
Ritesh Kumar <riteshk8055@gmail.com>
2023-08-22 18:25:01 +0530

    used externl script to generate qr code

--- script.js
+++ script.js
@@ -10,7 +10,28 @@ let size = sizes.value;
 generateBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     isEmptyInput();
+
 });
+
+sizes.addEventListener('change', (e)=>{
+
+    size = e.target.value;
+    isEmptyInput();
+})
+
+function isEmptyInput(){
+
+    if(qrText.value.length>0){
+
+        generateQRCode();
+
+    }
+
+    else{
+
+        alert("enter the text or url to generate your qr code");
+    }
+}
 
 function generateQRCode(){
     qrContainer.innerHTML = "";
